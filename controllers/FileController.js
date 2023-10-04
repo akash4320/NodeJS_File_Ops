6ee9400f75be419a51912b3cac28dbbf32999d40
akash4320 <akash4320@gmail.com>
2023-10-04 14:54:56 +0530

    File Operations Read/Write/ Append Data

--- controllers/FileController.js
+++ controllers/FileController.js
@@ -1,10 +1,49 @@
-const fs = require('fs');
+const fs = require("fs");
 
 exports.getDataFromFile = (req, res) => {
-    fs.readFile('HtmlFile.html', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
-        return res.end();
-    })
+  fs.readFile(req.body.filename, function (err, data) {
+    if (err) {
+      res(err);
+    } else {
+      res.writeHead(200, { "Content-Type": "text" });
+      res.write(data);
+      return res.end();
+    }
+  });
+};
 
-}
+exports.appendDataToFile = (req, res) => {
+  fs.appendFile(req.body.filename, req.body.data, function (err, data) {
+    if (err) {
+      res(err);
+    } else {
+      res.writeHead(200, { "Content-Type": "text" });
+      res.write("Sucessfully appended");
+      return res.end();
+    }
+  });
+};
+
+exports.writeDataToFile = (req, res) => {
+  fs.writeFile(req.body.filename, req.body.data, function (err, data) {
+    if (err) {
+      res(err);
+    } else {
+      res.writeHead(200, { "Content-Type": "text" });
+      res.write("Sucessfully writeFile");
+      return res.end();
+    }
+  });
+};
+
+exports.deleteFile = (req, res) => {
+  fs.unlink(req.body.filename, function (err, data) {
+    if (err) {
+      res(err);
+    } else {
+      res.writeHead(200, { "Content-Type": "text" });
+      res.write("Sucessfully deleted");
+      return res.end();
+    }
+  });
+};
--- routes/FileRoutes.js
+++ routes/FileRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const {
     getDataFromFile,
+    appendDataToFile,
+    writeDataToFile,
+    deleteFile
 } = require("../controllers/FileController");
 
 const router = express.Router();
 
-router.route("/").get(getDataFromFile);
+router.route("/").get(getDataFromFile).put(appendDataToFile).delete(deleteFile)
+router.route('/write').post(writeDataToFile)
 
 
 module.exports = router;
